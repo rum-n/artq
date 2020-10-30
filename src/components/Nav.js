8bfeb75ce807f476ca918a3ed3ec9bcba0a11bb2
rum-n <manev.ru@gmail.com>
2020-10-30 20:24:03 +0200

    added About component + some clean up

---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { Button,Nav,FormControl,Form } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import auth0Client from './../Auth';
+import './styles.css'
+
+const Navigation = () => { 
+
+    const signinButton = () => {
+        auth0Client.signIn();
+    }
+
+    const signOut = () => {
+        auth0Client.signOut();
+        this.props.history.replace('/');
+    }
+  return (
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">ArtQ</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Link className='nav-links' to='/'>My Feed</Link>
+          <Link className='nav-links' to='/latest'>Latest and Popular</Link>
+          <Link className='nav-links' to='/pricing'>Near Me</Link>
+          <Link className='nav-links' to='/about'>About</Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-primary">Search</Button>
+          {!auth0Client.isAuthenticated() && <Button className="btn btn-dark" onClick={() => { signinButton() }}>Sign In</Button>}
+          {auth0Client.isAuthenticated() && <label className="mr-2 text-white">{auth0Client.getProfile().name}</label>}
+          {auth0Client.isAuthenticated() && <Button className="btn btn-dark" onClick={() => { signOut() }}>Sign Out</Button>}
+        </Form>
+      </Navbar>
+  )
+}
+
+export default Navigation;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const About = () => {
+
+    return (
+        <div className="paragraphs">
+            <p className='about'>At one time artist Chui Faising didn’t worry about galleries or museums. Almost every sculpture and drawing he made was sold to selected patrons, and sometimes before he finished it. However, he saw that the art world was changing with the internet and social media.</p>
+            <p className='about'>The new technologies offered artists ways to market themselves and their work that was never available before. Instead of waiting for a few art patrons to visit your studio or see your work in an exhibition, with a little initiative, any artist could control over what was seen and sold of his work. They say that great ideas are born from simple needs and the great idea of ArtQ was born.</p>
+            <p className='about'>ArtQ is a new way of sharing and discovering art for free.Using our app from your mobile, tablet or computer, it is open for all artists to share their work, and it allows art lovers to find new art by style and location. It's better than relying on generic social media sites because you can see artwork near (and far), not just from people in your network. It’s an easy way to upload your images and bio. You don’t have to divulge your private information to the site. And best of all, you won’t be paying a fee and all the money from the sale of your art goes to the artist. It works whether you have a gallery or not. You arrange the sale and accept the payment.</p>
+            <p className='about'>As Chui likes to say, “It’s a very American concept; ArtQ is of the artist, for the artist and by the artist.” And finally, ArtQ makes loving art more fun and accessible for all. ArtQ is simply an art idea whose time has come!</p>
+        </div>
+    )
+}
+
+export default About;
