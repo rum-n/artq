d14229cc85746d875e5d599de063ce765b1064d6
Rumen Manev <r.manev@blubito.com>
2020-11-05 12:28:09 +0200

    Feed Modal - WIP

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,30 +1,62 @@
 import React, { useState } from "react";
 import PaypalButtons from "../pages/paypal";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import data from "./data";
 import './styles.css';
 
 const Feed = () => {
   const [showPaypal, setShowPaypal] = useState(false); 
   const [images, setImages] = useState(data);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const showPaypalButtons = () => {
     setShowPaypal(true);
   }
 
   return (
-    showPaypal ? <PaypalButtons /> : images.map(painting => (
-      <div className='outer-artwork'>
+    <>
+    {showPaypal ? <PaypalButtons /> : images.map(painting => (
+      <div className='outer-artwork' onClick={handleShow}>
         <div key={painting.title} className="artwork-card">
             <img src={painting.url} alt={painting.title}/>
             <p>Title: {painting.title}</p>  
             <p>Author: {painting.author}</p>  
             <p>Cost: {painting.cost}</p>
             <button onClick={showPaypalButtons}> Pay </button>
-          <br></br>
         </div>
-      </div>))
-    );
- 
+        <Modal dialogClassName="modal-100w" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{painting.author}</Modal.Title>
+        </Modal.Header>
+            <div className='modal-content'>
+              <div className='modal-img'>
+                <img src={painting.url} alt={painting.title}/>
+              </div>
+              <div className='modal-info'>
+                <h3>{painting.title}</h3>
+                <p>{painting.dimensions}</p>
+                <p><b>Description</b></p>
+                <p>{painting.description}</p>
+              
+              <Button variant="secondary">
+                Add to cart
+              </Button>
+              <Button variant="primary">
+                Save for later
+              </Button>
+              </div>
+            </div>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
+      </div>))}
+
+      </>
+    ); 
   }
 
 export default Feed;
--- src/components/data.js
+++ src/components/data.js
@@ -1,31 +1,3 @@
-{/* This file is used by Home.js */}
-// export default {
-//     FAKE: 0,
-//     REAL: 0,
-//     author: 'USER',
-//     caption: 'CAPTION',
-//     photoId: '47d5-9420-c99a-7459-640d',
-//     posted: '72 minutes ago',
-//     url: [
-//       {
-//         url:
-//           'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg', cost: '$400',
-//       },
-//       {
-//         url:
-//           'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', cost: '$500',
-//       },
-//       {
-//         url:
-//           'https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2018/2-dog.jpg', cost: '$100',
-//       },
-//       {
-//         url: 'https://www.guidedogs.org/wp-content/uploads/2018/01/Mobile.jpg', cost: '$200',
-//       },
-//     ],
-//   };
-  
-
 export default [
   {
     "title": "PaintingTitle1",
@@ -35,7 +7,9 @@ export default [
     "location": { 
       lat: 41.3954,
       lng: 2.162 
-    }
+    },
+    "dimensions": "H 200 cm x W 125 cm",
+    "description": "A cute puppy"
   },
   {
     "title": "PaintingTitle2",
@@ -45,7 +19,9 @@ export default [
     "location": { 
       lat: 10.3954,
       lng: 2.162 
-    }
+    },
+    "dimensions": "H 200 cm x W 125 cm",
+    "description": "Two puppies."
   },
   {
     "title": "PaintingTitle3",
@@ -55,7 +31,9 @@ export default [
     "location": { 
       lat: 60.3954,
       lng: 2.162 
-    }
+    },
+    "dimensions": "H 200 cm x W 125 cm",
+    "description": "Older dog."
   },
   {
     "title": "PaintingTitle4",
@@ -65,6 +43,8 @@ export default [
     "location": { 
       lat: 20.3954,
       lng: 2.162 
-    }
+    },
+    "dimensions": "H 200 cm x W 125 cm",
+    "description": "A cute puppy, again."
   }
 ]
