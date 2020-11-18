73b4b47d768ffc48df562edb3dababcc1c9f9969
rum-n <manev.ru@gmail.com>
2020-11-18 21:21:48 +0200

    feed cards

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 import data from "./data";
 import './styles.css';
 
@@ -24,53 +26,41 @@ const Feed = () => {
 
   return (
     <>
-    {showPaypal ? <PaypalButtons /> : images.map(painting => (
-      <div className='outer-artwork' onClick={handleShow}>
-        <div 
-          key={painting.title} 
-          className="artwork-card"
-          onMouseEnter={handleMouseHover}
-          onMouseLeave={()=>setIsHovering(false)}>
-            <img src={painting.url} alt={painting.title}/>
-            {isHovering && <div className="hover-info">
-              <p>{painting.author}</p>  
-              <p>{painting.title}</p>  
-              <p>{painting.dimensions}</p>
-            </div>}
-            <button onClick={showPaypalButtons}> Pay </button>
-        </div>
-
-        {/* This stupid Modal is acting all weird and I can't figure out why!
-        For some reason, it's only showing the last entry in the data file. */}
-
-        <Modal dialogClassName="modal-100w" show={show} onHide={handleClose} backdrop="false">
-
-        <Modal.Header closeButton>
-          <Modal.Title>{painting.author}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Container>
-            <Row>
-              <Col xs={12} md={6}>
-                  <img className="modal-img" src={painting.url} alt={painting.title}/>
-              </Col>
-              <Col xs={12} md={6}>
-                <h3>{painting.title}</h3>
-                <p>{painting.dimensions}</p>
-                <p><b>Description</b></p>
-                <p>{painting.description}</p>              
-                <Button className="add-to-cart" variant="secondary">Add to cart</Button>
-                <Button className="save-for-later" variant="primary">Save for later</Button>
-              </Col>
-            </Row>
-          </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <a className='see-more'>See more</a>
-        </Modal.Footer>
-      </Modal>
-      </div>))}
-
+      {showPaypal ? <PaypalButtons /> : <CardDeck>
+        {images.map(painting => (
+          <Col xs={1} md={4}>
+            <Card key={painting.title} style={{ width: '25rem', marginBottom: '2rem' }} onClick={handleShow}>
+              <Card.Img src={painting.url} />
+                <Card.Text>
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>{painting.author}</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Container>
+                      <Row>
+                        <Col xs={12} md={6}>
+                            <img className="modal-img" src={painting.url} alt={painting.title}/>
+                        </Col>
+                        <Col xs={12} md={6}>
+                          <h3>{painting.title}</h3>
+                          <p>{painting.dimensions}</p>
+                          <p><b>Description</b></p>
+                          <p>{painting.description}</p>              
+                          <Button className="add-to-cart" variant="secondary">Add to cart</Button>
+                          <Button className="save-for-later" variant="primary">Save for later</Button>
+                        </Col>
+                      </Row>
+                    </Container>
+                  </Modal.Body>
+                    <Modal.Footer>
+                      <a className='see-more'>See more</a>
+                    </Modal.Footer>
+                </Modal>
+                </Card.Text>     
+            </Card>
+          </Col>))}
+        </CardDeck>}
       </>
   )}
   
