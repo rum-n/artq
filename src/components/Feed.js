782685b5b166eadd477713f499699137308fea48
rum-n <manev.ru@gmail.com>
2020-11-19 21:45:25 +0200

    Feed modal works

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -14,24 +14,18 @@ const Feed = () => {
   const [showPaypal, setShowPaypal] = useState(false); 
   const [images] = useState(data);
   const [show, setShow] = useState(false);
+  const [activeItem, setActiveItem] = useState('');
   const [isHovering, setIsHovering] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const handleMouseHover = () => setIsHovering(true);
+  const handleShow = (item) => {
+    setActiveItem(item)
+    setShow(true)
+ };
 
   const showPaypalButtons = () => {
     setShowPaypal(true);
   }
-
-  // const PaintingsList = () => images.map(painting => (
-  //   <CardDeck>
-  //     <Col key={painting.id} xs={1} md={4}>
-  //       <Card style={{ width: '25rem', marginBottom: '2rem' }} onClick={handleShow}>
-  //         <Card.Img src={painting.url} /> 
-  //       </Card>
-  //     </Col>
-  //   </CardDeck>))
 
   return (
     <React.Fragment>
@@ -39,24 +33,26 @@ const Feed = () => {
       {showPaypal ? <PaypalButtons /> : <CardDeck>
         {images.map(painting => 
           <Col key={painting.id} xs={1} md={4}>
-            <Card style={{ width: '25rem', marginBottom: '2rem' }} onClick={handleShow}>
+            <Card style={{ width: '25rem', marginBottom: '2rem' }} onClick={() => handleShow(painting)}>
               <Card.Img src={painting.url} /> 
             </Card>
+          </Col>)}
+        </CardDeck>}
             <Modal show={show} onHide={handleClose}>
                   <Modal.Header closeButton>
-                    <Modal.Title>{painting.author}</Modal.Title>
+                    <Modal.Title>{activeItem.author}</Modal.Title>
                   </Modal.Header>
                   <Modal.Body>
                     <Container>
                       <Row>
                         <Col xs={12} md={6}>
-                            <img className="modal-img" src={painting.url} alt={painting.title}/>
+                            <img className="modal-img" src={activeItem.url} alt={activeItem.name}/>
                         </Col>
                         <Col xs={12} md={6}>
-                          <h3>{painting.title}</h3>
-                          <p>{painting.dimensions}</p>
+                          <h3>{activeItem.title}</h3>
+                          <p>{activeItem.dimensions}</p>
                           <p><b>Description</b></p>
-                          <p>{painting.description}</p>              
+                          <p>{activeItem.description}</p>              
                           <Button className="add-to-cart" variant="secondary">Add to cart</Button>
                           <Button className="save-for-later" variant="primary">Save for later</Button>
                         </Col>
@@ -67,36 +63,7 @@ const Feed = () => {
                       <a className='see-more'>See more</a>
                     </Modal.Footer>
                 </Modal>
-          </Col>)}
-        </CardDeck>}
       </React.Fragment>
   )}
   
 export default Feed;
-
-//  const movies = {} 
- 
-// const MovieList = ({movies}) =>
-// movies.map((movie, index) => 
-// (<ListGroup.Item key={movie.imdbID}><Row>
-//   <Col md={2}>{movie.Year}</Col>
-//   <Col md={3}>{movie.imdbID}</Col>
-//   </Row>
-// </ListGroup.Item>));  
-// render (
-  // <div>
-  //     <p onClick={this.props.onShowModal}>{this.props.movie.Title}{this.props.index}</p>
-  //     <Modal show={this.props.modal} onHide={this.props.onHideModal} backdropClassName={'modal-backdrop'}>
-  //     <Modal.Header closeButton>
-  //       <Modal.Title>{this.props.movie.Title} {this.props.index}</Modal.Title>
-  //       </Modal.Header>
-  //       <Modal.Body
-  //         <ListGroup>{/* <----- put your list const here */}
-  //         <MovieList movies={movies} />
-  //         </ListGroup>
-  //       </Modal.Body>
-  //       <Modal.Footer>
-  //         <h1>test footer</h1>
-  //       </Modal.Footer>
-  //     </Modal>             
-  // </div>  
