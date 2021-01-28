e5a7e8c8b00b7a14f49201d85dc3c4b95aeb7792
Rumen Manev <r.manev@blubito.com>
2021-01-28 23:13:15 +0200

    sidenav menu

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,12 +1,36 @@
-import React, {useContext, useState} from 'react';
+import React, { useState, useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SideNav.css';
+import { Link } from 'react-router-dom';
+import {AuthContext} from "../context/auth-context"
 
 const SideNav = () => {
+    const [ hover, setHover ] = useState(false);
+    const auth = useContext(AuthContext)
+    let linkStyle;
+
+    const mouseOver = () => {
+        setHover(true);
+        linkStyle = {
+            cursor: 'pointer'
+        }
+    };
+
+    const mouseLeave = () => {
+        setHover(false);
+    };
     
     return (
-        <div>
-            
+        <div className='sidenav'>
+            <ul>
+                <li style={linkStyle} onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>Followed</li>
+                <li onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>Explore</li>
+                <li onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>Current bids</li>
+                <li onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>Saved</li>
+                <li onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>Near me</li>
+            </ul>
+            <Link to={auth.isLoggedIn ? '/add-art' : 'signup'}><button className='add-art'>Add Art <span>+</span></button></Link>
+            {/* <icon/> */}
         </div>
     )
 }
